cd45b3952d2d129e28a5ef7c74eb278cd1c9b97f
SoJung <33796437+sjham16@users.noreply.github.com>
2019-12-03 19:33:39 -0500

    yay

--- pokemans.py
+++ pokemans.py
@@ -3,7 +3,7 @@ import json
 import os
 import unittest
 import pokebase as pb
-
+import sqlite3
 
 
 
@@ -11,20 +11,20 @@ import pokebase as pb
 #Project Members: SoJung Ham, Dylan Yono
 #Information: 
 
-# def setUpDatabase(db_name):
+def setUpDatabase(db_name):
     
 
-    # conn = sqlite3.connect(db_name)
-    # cur = conn.cursor()
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
 
-    # return cur, conn
+    return cur, conn
 
 def get_pokemon_data():
 
     try:
-        url = "https://pokeapi.co/api/v2/pokemon/" #probably need to limit this to 151 pokemon only
+        url = "https://pokeapi.co/api/v2/pokemon/1" #probably need to limit this to 151 pokemon only
         r = requests.get(url)
-        myDict = json.loads(r)
+        myDict = json.loads(r.text)
         # pp = pprint.PrettyPrinter(indent=4)
         # pp.pprint(myDict)
         #getting stats is 
@@ -38,29 +38,31 @@ def get_pokemon_data():
 
 
 #Fun table stuff
-# def setUpPokemonBaseStatsTable(pokemon_data, cur, conn):
+def setUpPokemonBaseStatsTable(pokemon_data, cur, conn):
     
-#     cur.execute("DROP TABLE IF EXISTS Pokemon")
-#     cur.execute('''CREATE TABLE Pokemon (pokemon_id INTEGER PRIMARY KEY, 
-#                                         speed INTEGER, special_defense INTEGER, special_attack INTEGER, 
-#                                         defense INTEGER, attack INTEGER, hp INTEGER)''')
-#     info = pokemon_data['stats']
+    cur.execute("DROP TABLE IF EXISTS Pokemon")
+    cur.execute('''CREATE TABLE Pokemon (pokemon_id INTEGER, 
+                                        speed INTEGER, special_defense INTEGER, special_attack INTEGER, 
+                                        defense INTEGER, attack INTEGER, hp INTEGER)''')
+    info = pokemon_data
 
-#     for stat in info:
-#         _pokemon_id = info['id']
-#         _speed = info[0]['base_stat']
-#         _special_defense = info[1]['base_stat']
-#         _special_attack = info[2]['base_stat']
-#         _defense = info[3]['base_stat']  
-#         _attack = info[4]['base_stat'] 
-#         _hp = info[5]['base_stat'] 
+    for _ in info:
+        _pokemon_id = info['id']
+        _speed = info['stats'][0]['base_stat']
+        _special_defense = info['stats'][1]['base_stat']
+        _special_attack = info['stats'][2]['base_stat']
+        _defense = info['stats'][3]['base_stat']  
+        _attack = info['stats'][4]['base_stat'] 
+        _hp = info['stats'][5]['base_stat'] 
 
-#         cur.execute('INSERT INTO Pokemon (pokemon_id, speed, special_defense, special_attack, defense, attack, hp) VALUES (?, ?, ?, ?, ?, ?, ?)', (_pokemon_id, _speed, _special_defense, _special_attack, _defense, _attack, _hp))
-#     conn.commit()
-#create a cache of the first 151 pokemon
-#get all of their stats in a nice table
+        cur.execute('INSERT INTO Pokemon (pokemon_id, speed, special_defense, special_attack, defense, attack, hp) VALUES (?, ?, ?, ?, ?, ?, ?)', (_pokemon_id, _speed, _special_defense, _special_attack, _defense, _attack, _hp))
+    conn.commit()
+# create a cache of the first 151 pokemon
+# get all of their stats in a nice table
 # calculate the average of each typing 
 
-def main():
-    print(get_pokemon_data())
-    # cur, conn = setUpDatabase('Pokemon.sqlite')
+pokemon_data = get_pokemon_data()
+cur, conn = setUpDatabase('Pokemon.db')
+setUpPokemonBaseStatsTable(pokemon_data, cur, conn)
+
+
