77ff53de4969732fc9bd5d27d5c854918eb1013b
SoJung <33796437+sjham16@users.noreply.github.com>
2019-12-11 15:39:43 -0500

    i literally did the entire project by myself lmfao

--- starter.py
+++ starter.py
@@ -6,7 +6,8 @@ import sqlite3
 import time
 
 
-from jikanpy import Jikan
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 
@@ -45,58 +46,34 @@ def write_cache(cache_file, cache_dict):
     fw.write(dumped_json_cache) #write the json into the cache 
     fw.close() #close the filw 
     
-def get_data_with_caching():
+def get_data_with_caching(year):
 
-    api_anime_code = 1
 
-    for anime in range(5):
-
-        base_url = "https://api.jikan.moe/v3/search/anime?q=2013-2018&limit=101"
-        #FIX this url
-        request_url = base_url.format(api_anime_code)
+      
+    base_url = "https://api.jikan.moe/v3/search/anime?q={}&limit=101"
+    request_url = base_url.format(year)
     
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        CACHE_FNAME = dir_path + '/' + "jikan_cache.json"
-        CACHE_DICTION  = read_cache(CACHE_FNAME)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    CACHE_FNAME = dir_path + '/' + "jikan_cache.json"
+    CACHE_DICTION  = read_cache(CACHE_FNAME)
 
-        try:
-            if request_url in CACHE_DICTION:
-        
+    try:
+        if request_url in CACHE_DICTION:
+            
                 print("Using jikan anime_list cache")
-            
-
                 return CACHE_DICTION[request_url]
-        
-        
-        
-            else: 
-                #if request_url does not exist in CACHE_DICTION, CREATE THE CACHE
-                print("Fetching")
-                ugh = requests.get(request_url)
-                all_anime = json.loads(ugh.text)
-
-            # myDict = {}
-                     
-            # number = 1
-            # for x in all_anime['results']:
-            #     url = x["url"]
-            #     r = requests.get(url)
-            #     info = json.loads(r.text)
-            #     myDict[number] =  info
-            #     number += 1
             
-            
-            
-                CACHE_DICTION[request_url] = all_anime
-                #write out the dictionary to a file using the function write_cache   
-                write_cache(CACHE_FNAME, CACHE_DICTION)
+        else: 
+                    #if request_url does not exist in CACHE_DICTION, CREATE THE CACHE
+            print("Fetching")
+            ugh = requests.get(request_url)
+            all_anime = json.loads(ugh.text)
 
-                api_anime_code += 1
-
-                
-        except:
-            print("Exception")
-            return None
+            CACHE_DICTION[request_url] = all_anime
+            write_cache(CACHE_FNAME, CACHE_DICTION)                    
+    except:
+        print("Exception")
+        return None
     
     return all_anime
     
@@ -151,13 +128,59 @@ def setUpRatedTable(anime_data, cur, conn):
         cur.execute("INSERT INTO Rated (id, rated) VALUES (?,?)", (i, genre_list[i]))
     conn.commit()
 
-            
+#----------joining tables ----------------------
+def getAnimeScoreAndRatings(cur, conn):
+    cur.execute('''SELECT Anime.name, Anime.score FROM Anime INNER JOIN Rated ON Anime.rated_id = Rated.id''')
+    lst = cur.fetchall()
+    return lst
 
-anime_data = get_data_with_caching()
+#------calculation stuff------------------------
+def getAverageScore(rated, cur, conn):
+
+    cur.execute("SELECT id FROM Rated WHERE rated = ? LIMIT 1", (rated, ))
+    rated_id = cur.fetchall()[0][0]
+    
+    cur.execute("SELECT score FROM Anime WHERE rated_id = ?", (rated_id, ))
+    average = 0
+    total = 0
+    resu = cur.fetchall()
+
+    for anime in resu:
+        total = anime[0] + total
+    average = total / len(resu)
+
+    return average
+#----------------creating a graph-------------------
+def createAverageScoreGraph():
+    scoreStats = {}
+
+    rating_types = ['G','PG', 'PG-13', 'R', 'R+', 'Rx']
+
+    for _type in rating_types:
+        scoreStats[_type] = getAverageScore(_type, cur, conn)
+
+    plt.bar(scoreStats.keys(), scoreStats.values())
+
+    plt.ylabel('Average Score out of 10')
+    plt.xlabel('Rated')
+    plt.title('Does a Rating determine how well it scores?')
+    plt.show()
+#----------set up-----------------------------
+anime_data = get_data_with_caching(2011)
 cur, conn = setUpDatabase('Anime.db')
 
 setUpRatedTable(anime_data, cur, conn)
 setUpAnimeInfoTable(anime_data, cur, conn)
+
+print(getAverageScore('R', cur, conn))
+createAverageScoreGraph()
+
+#-------------------------------------------------------
+
+# with open('anime_calc.csv', 'c', newline='') as csvfile:
+#     fields = ['anime_id', 'name', 'average score', 'rated']
+
+#please select what year you want to compare to .... ?
 
 
 
